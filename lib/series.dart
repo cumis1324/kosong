d0746c761a1bc98dd7dd0dd564afb5dcfdc9cdf3
cumis <nfgplus@gmail.com>
2024-06-28 03:40:59 +0700

    fix

--- lib/series.dart
+++ lib/series.dart
@@ -5,7 +5,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 const INDEX_URLS = [
-  'https://drive3.nfgplusmirror.workers.dev/0:/Series/'
+  'https://drive2.nfgplusmirror.workers.dev/0:/Series/'
   // Add more index URLs as needed
 ];
 const TMDB_API_KEY = '75399494372c92bd800f70079dff476b';
@@ -23,44 +23,53 @@ Future<void> handleRequest() async {
     bool hasNextPage = true;
 
     while (hasNextPage) {
-      final data = await fetchScraperData(indexUrl, nextPageToken, pageIndex);
-      final files = data['data']['files'];
+      try {
+        final data = await fetchScraperData(indexUrl, nextPageToken, pageIndex);
+        if (data == null || !data.containsKey('data') || !data['data'].containsKey('files')) {
+          throw Exception('Invalid data structure received from $indexUrl');
+        }
 
-      for (final file in files) {
-        if (file['mimeType'] != 'application/vnd.google-apps.folder') {
-          final extractedData = extractNameAndQuality(file['name']);
-          if (extractedData != null) {
-            try {
-              final tmdbData = await fetchTmdbData(extractedData['name']!, extractedData['seasonNumber']!, TMDB_API_KEY);
-              if (tmdbData.isNotEmpty) {
-                await storeToFirestore(
-                  tmdbData,
-                  extractedData['seasonNumber']!,
-                  extractedData['episode']!,
-                  file['name'],
-                  indexUrl + Uri.encodeComponent(file['name']),
-                  file['modifiedTime'],
-                  file['size'],
-                  file['mimeType'],
-                  extractedData['qualityName']!,
-                  extractedData['qualityVideo']!
-                );
-                print('Added File ${file['name']} to Firestore');
-              } else {
-                print('No TMDB data found for ${extractedData['name']} (${extractedData['seasonNumber']})');
+        final files = data['data']['files'];
+
+        for (final file in files) {
+          if (file != null && file['mimeType'] != null && file['mimeType'] != 'application/vnd.google-apps.folder') {
+            final extractedData = extractNameAndQuality(file['name']);
+            if (extractedData != null) {
+              try {
+                final tmdbData = await fetchTmdbData(extractedData['name']!, extractedData['seasonNumber']!, TMDB_API_KEY);
+                if (tmdbData.isNotEmpty) {
+                  await storeToFirestore(
+                    tmdbData,
+                    extractedData['seasonNumber']!,
+                    extractedData['episode']!,
+                    file['name'],
+                    indexUrl + Uri.encodeComponent(file['name']),
+                    file['modifiedTime'] ?? '',
+                    file['size'] ?? '',
+                    file['mimeType'] ?? '',
+                    extractedData['qualityName']!,
+                    extractedData['qualityVideo']!
+                  );
+                  print('Added File ${file['name']} to Firestore');
+                } else {
+                  print('No TMDB data found for ${extractedData['name']} (${extractedData['seasonNumber']})');
+                }
+              } catch (e) {
+                print('Error fetching TMDB data for ${extractedData['name']} (${extractedData['seasonNumber']}): $e');
               }
-            } catch (e) {
-              print('Error fetching TMDB data for ${extractedData['name']} (${extractedData['seasonNumber']}): $e');
             }
           }
         }
-      }
 
-      if (data.containsKey('nextPageToken')) {
-        nextPageToken = data['nextPageToken'];
-        pageIndex++;
-      } else {
-        hasNextPage = false;
+        if (data.containsKey('nextPageToken')) {
+          nextPageToken = data['nextPageToken'] ?? '';
+          pageIndex++;
+        } else {
+          hasNextPage = false;
+        }
+      } catch (e) {
+        print('Error processing index URL $indexUrl: $e');
+        hasNextPage = false; // Stop processing further pages on error
       }
     }
   }
@@ -68,29 +77,34 @@ Future<void> handleRequest() async {
   print('Data processing complete');
 }
 
-Future<Map<String, dynamic>> fetchScraperData(String url, String nextPageToken, int pageIndex) async {
-  final response = await http.post(
-    Uri.parse(url),
-    headers: {
-      HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
-    },
-    body: {
-      'page_token': nextPageToken,
-      'page_index': pageIndex.toString(),
-    },
-  );
+Future<Map<String, dynamic>?> fetchScraperData(String url, String nextPageToken, int pageIndex) async {
+  try {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
+      },
+      body: {
+        'page_token': nextPageToken,
+        'page_index': pageIndex.toString(),
+      },
+    );
 
-  if (response.statusCode != 200) {
-    if (response.statusCode == 404) {
-      throw Exception('Resource not found: $url');
-    } else {
-      throw Exception('Request failed with status ${response.statusCode}');
+    if (response.statusCode != 200) {
+      if (response.statusCode == 404) {
+        throw Exception('Resource not found: $url');
+      } else {
+        throw Exception('Request failed with status ${response.statusCode}');
+      }
     }
-  }
 
-  final encryptedResponse = response.body;
-  final decryptedResponse = decryptResponse(encryptedResponse);
-  return jsonDecode(decryptedResponse);
+    final encryptedResponse = response.body;
+    final decryptedResponse = decryptResponse(encryptedResponse);
+    return jsonDecode(decryptedResponse);
+  } catch (e) {
+    print('Error fetching scraper data for $url: $e');
+    return null;
+  }
 }
 
 String decryptResponse(String response) {
@@ -117,12 +131,12 @@ Future<Map<String, dynamic>> fetchTmdbData(String name, String seasonNumber, Str
       'seasonData': tmdbSeasonData,
     };
   } else {
-    return {}; // Return empty map if no results found
+    throw Exception('No TMDB results found for query $name');
   }
 }
 
 Future<Map<String, dynamic>> fetchTmdbSeasonData(String seriesId, String seasonNumber, String apiKey) async {
-  final tmdbUrl = 'https://api.themoviedb.org/3/tv/$seriesId/season/$seasonNumber?api_key=$apiKey&language=en-US';
+  final tmdbUrl = 'https://api.themoviedb.org/3/tv/$seriesId/season/$seasonNumber?api_key=$apiKey';
   final response = await http.get(Uri.parse(tmdbUrl));
 
   if (response.statusCode != 200) {
@@ -178,11 +192,14 @@ Future<void> storeToFirestore(Map<String, dynamic> tmdbData, String seasonNumber
 
 String findEpisodeId(List<dynamic> episodes, String episodeNumber) {
   for (final episode in episodes) {
-    if (episode['episode_number'].toString() == episodeNumber) {
-      return episode['id'].toString();
+    final episodeNumberStr = episode['episode_number']?.toString();
+    final episodeId = episode['id']?.toString();
+
+    if (episodeNumberStr == episodeNumber && episodeId != null) {
+      return episodeId;
     }
   }
-  
+
   // Handle case where no matching episode ID is found
   throw Exception('Episode $episodeNumber not found in the episodes list.');
 }
@@ -193,7 +210,7 @@ Map<String, String>? extractNameAndQuality(String filename) {
     caseSensitive: false,
   );
   final match = regExp.firstMatch(filename);
-  
+
   if (match != null) {
     final initialDigits = match.group(1);
     final name = '${initialDigits ?? ''} ${match.group(2)?.replaceAll('.', ' ')}'.trim(); // Concatenate and replace dots with spaces
@@ -201,6 +218,13 @@ Map<String, String>? extractNameAndQuality(String filename) {
     final episode = match.group(4); // Episode info (E1)
     final qualityVideo = match.group(5); // Quality (720p)
     final qualityName = match.group(6); // Remaining info (Blueray, etc.)
+
+    print('Extracted details:');
+    print('Name: $name');
+    print('Season Number: $seasonNumber');
+    print('Episode: $episode');
+    print('Quality Video: $qualityVideo');
+    print('Quality Name: $qualityName');
 
     if ((initialDigits != null || match.group(2) != null) && seasonNumber != null && episode != null && qualityVideo != null && qualityName != null) {
       return {
@@ -215,6 +239,7 @@ Map<String, String>? extractNameAndQuality(String filename) {
 
   return null;
 }
+
 
 void main() {
   handleRequest().catchError((error) {
